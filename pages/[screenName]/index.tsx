b76e9e4b65c91bf485ec02c51fcdca6e60b3f914
Lenson-Lee <dmstjs7437@naver.com>
2023-02-21 17:44:01 +0900

    나의서재 데이터 뽑기 완료

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,10 +1,23 @@
+import MyBookList from "@/components/List/mybookList";
 import ServiceLayout from "@/components/service_layout";
+import { useAuth } from "@/contexts/auth_user.context";
 import { NextPage } from "next";
 
 const UserHomePage: NextPage = function () {
+  const { authUser } = useAuth();
   return (
     <ServiceLayout>
-      <div className="bg-black h-10"></div>
+      <div className="bg-white w-full">
+        <section className="py-20 px-10">
+          <div className="flex items-end mb-8">
+            <div className="text-2xl font-bold mr-8">내가 저장한 책</div>
+            <div className="text-lg font-bold mr-4">다 읽은 책</div>
+            <div className="text-lg font-bold mr-4">읽고 있는 책</div>
+            <div className="text-lg font-bold mr-4">찜한 책</div>
+          </div>
+          <MyBookList userData={authUser} />
+        </section>
+      </div>
     </ServiceLayout>
   );
 };
